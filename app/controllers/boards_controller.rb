bd1d8741a915cebf2cc679916b804c849a726788
serenanamee <serena.chang169@gmail.com>
2020-07-24 16:35:29 +0800

    layout

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,12 +1,53 @@
 class BoardsController < ApplicationController
+
+
+
+
 def index
   @boards = Board.all
 end
 def new
 
+  @board = Board.new
+
 end
-def create
-  render html: params
+   def create
+
+  @board = Board.new (border_params)
+  if @board.save  
+     redirect_to root_path, notice: "Successfully created a new board" 
+  
+  else
+    render 'new'
+  end
+end
+
+def show
+      @board = Board.find(params[:id])
+end
+
+def edit
+  @board = Board.find(params[:id])
+
+end
+
+def update
+  @board = Board.find(params[:id])
+
+  if @board.update(board_params)
+     redirect_to boards_path, notice: "update"
+  
+  else
+    render :edit
+  end
+
+end
+
+
+private
+
+def border_params
+  params.require(:board).permit(:title, :intro)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "pages#index"
+  root "boards#index"
 
   get "/about", to: "pages#about"
 
   resources :boards
 end
+
+
+
